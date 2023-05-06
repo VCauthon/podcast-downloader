54bd3d0c405bfd820268a88fca370cea11d90758
(´-`)ノ <victormasmon@gmail.com>
2023-05-06 07:47:32 +0200

    Function to make a resume with GPT

--- podcast_downloader/summary.py
+++ podcast_downloader/summary.py
@@ -4,3 +4,12 @@ text from ChatGPT based on the key seconds provided by the user. Here,
 functions could be defined to send the transcription to ChatGPT and
 receive a summary response.
 """
+import requests
+
+# function to request summary from ChatGPT
+def get_summary(text):
+    # send the text to ChatGPT API and receive summary response
+    response = requests.post(url="CHATGPT_API_URL", data={"text": text})
+    summary = response.text
+    return summary
+
