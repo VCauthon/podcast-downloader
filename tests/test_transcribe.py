07b641db3eccfa26ad9b9a41d828a0c9292a3acb
(´-`)ノ <victormasmon@gmail.com>
2023-05-14 21:01:42 +0200

    Initial structure test_transcribe.py

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -1,1 +1,25 @@
+import pytest
+from your_module import Transcribe
+
+@pytest.fixture
+def transcribe_instance():
+    video = "path/to/video.mp4"  # Provide a path to a sample video file
+    return Transcribe(video)
+
+def test_transcribe(transcribe_instance):
+    result = transcribe_instance.transcribe()
+    assert isinstance(result, str)
+    assert len(result) > 0
+
+def test_return_audio(transcribe_instance):
+    audio = transcribe_instance.RetrieveAudio.return_audio()
+    assert isinstance(audio, bytes)
+    assert len(audio) > 0
+
+def test_concrete_sections_of_the_video(transcribe_instance):
+    start_time = 10  # Provide a start time in seconds
+    end_time = 20  # Provide an end time in seconds
+    result = transcribe_instance.RetrieveAudio._Transcribe__concrete_sections_of_the_video(start_time, end_time)
+    assert isinstance(result, str)
+    assert len(result) > 0
 
