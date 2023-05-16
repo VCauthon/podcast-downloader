66f57a748782ea2ac5b9759d73dcace8ba508eeb
Victor Mas Montserrat <victormasmon@gmail.com>
2023-05-16 16:53:58 +0200

    Implementing youtube download

--- podcast_downloader/downloader/downloader.py
+++ podcast_downloader/downloader/downloader.py
@@ -1,27 +1,54 @@
 from abc import ABC, abstractmethod
+from io import BytesIO
+from pytube import YouTube
+
+
+class ContextVideoDownloaded:
+    def __init__(self, title: str, bytes: bytes, author: str, seconds: int):
+        self.title = title
+        self.author = author
+        self.seconds = seconds
+        self.bytes = bytes
 
 
 class interface_ABC(ABC):
     @abstractmethod
-    def download(self, url):
+    def download(self, url) -> ContextVideoDownloaded:
         pass
 
     @staticmethod
     def detect_platform(url: str) -> str:
-        return 'url.split(".")[1]'
+        return url.split(".")[1]
 
 
 class youtube(interface_ABC):
-    def download(self, url):
-        print("youtube")
+    def download(self, url) -> ContextVideoDownloaded:
+
+        # Create a YouTube object with the video URL
+        youtube = YouTube(url)
+
+        # Get the highest resolution video stream available
+        stream = youtube.streams.get_highest_resolution()
+
+        # Gets the video into a stream of bytes
+        bytes_video = BytesIO()
+        stream.stream_to_buffer(bytes_video)
+
+        # Download the video into a stream of bytes
+        return ContextVideoDownloaded(
+            title=youtube.title,
+            author=youtube.author,
+            seconds=youtube.length,
+            bytes=bytes_video.getvalue(),
+        )
 
 
 class spotify(interface_ABC):
-    def download(self, url):
+    def download(self, url) -> ContextVideoDownloaded:
         print("spotify")
 
 
-def downloader(url: str) -> bytes:
+def downloader(url: str) -> ContextVideoDownloaded:
     # Detects the type of platform
     platform = interface_ABC.detect_platform(url)
 
@@ -43,4 +70,6 @@ def downloader(url: str) -> bytes:
     ][0]().download(url)
 
 
-downloader("youtube.com")
+# TODO: Remove this
+if __name__ == "__main__":
+    downloader("youtube.com")
