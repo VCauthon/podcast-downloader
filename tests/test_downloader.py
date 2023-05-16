1d45e785bf5422bb8bf17fe881579d613862cd81
Victor Mas Montserrat <victormasmon@gmail.com>
2023-05-16 16:53:04 +0200

    Testing youtube download

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,32 +1,35 @@
-
-import pytest
-
 import sys
-sys.path.append('../src')
+
+sys.path.append("../src")
 from podcast_downloader.downloader import downloader  # noqa: E402
 
 
 def test_downloader_youtube():
-    url = "https://www.youtube.com/watch?v=PrVgdrsp-b8"
-    result = downloader(url)
-    assert isinstance(result, bytes)
-    assert len(result) > 0
+    url = (
+        "https://www.youtube.com/watch?v=52Gg9CqhbP8&list="
+        "PLlVaFbgtYyJfgQAoHqDjz5MDmVLwARcWo&index=3"
+    )
+    result = downloader.downloader(url)
+    assert (
+        isinstance(result.bytes, bytes) and len(result.bytes) > 0
+    ), "The download hasn't been successful"
+    assert result.title, "The download hasn't been successful"
 
 
-def test_downloader_spotify():
-    url = "https://open.spotify.com/track/7xGfFoTpQ2E7fRF5lN10tr"
-    with pytest.raises(ValueError):
-        downloader(url)
+# def test_downloader_spotify():
+#     url = "https://open.spotify.com/track/7xGfFoTpQ2E7fRF5lN10tr"
+#     with pytest.raises(ValueError):
+#         downloader(url)
 
 
-def test_detect_platform_youtube():
-    url = "https://www.youtube.com/watch?v=PrVgdrsp-b8"
-    platform = "__youtube"
-    result = downloader(url)
-    assert result == platform
+# def test_detect_platform_youtube():
+#     url = "https://www.youtube.com/watch?v=PrVgdrsp-b8"
+#     platform = "__youtube"
+#     result = downloader(url)
+#     assert result == platform
 
 
-def test_detect_platform_unsupported():
-    url = "https://example.com/video"
-    with pytest.raises(ValueError):
-        downloader(url)
+# def test_detect_platform_unsupported():
+#     url = "https://example.com/video"
+#     with pytest.raises(ValueError):
+#         downloader(url)
