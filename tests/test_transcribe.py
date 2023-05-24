031c49c800d459e75c23906ecbcdff9029a1ac8a
Victor Mas Montserrat <victormasmon@gmail.com>
2023-05-24 19:41:43 +0200

    Connect video to testing script

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -7,24 +7,23 @@ from podcast_downloader.transcribe import Transcribe  # noqa: E402
 
 
 @pytest.fixture
-def transcribe_instance():
-    video = "path/to/video.mp4"  # Provide a path to a sample video file
-    return Transcribe(video)
+def transcribe_instance() -> Transcribe:
+    return Transcribe("./tests/testing.mp4")
 
 
-def test_transcribe(transcribe_instance):
+def test_transcribe(transcribe_instance: Transcribe):
     result = transcribe_instance.transcribe()
     assert isinstance(result, str)
     assert len(result) > 0
 
 
-def test_return_audio(transcribe_instance):
+def test_return_audio(transcribe_instance: Transcribe):
     audio = transcribe_instance.RetrieveAudio.return_audio()
     assert isinstance(audio, bytes)
     assert len(audio) > 0
 
 
-def test_concrete_sections_of_the_video(transcribe_instance):
+def test_concrete_sections_of_the_video(transcribe_instance: Transcribe):
     start_time = 10  # Provide a start time in seconds
     end_time = 20  # Provide an end time in seconds
     result = (
