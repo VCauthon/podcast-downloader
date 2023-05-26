47d29b132945fefd61e502cebd408e867e3fdadb
Victor Mas Montserrat <victormasmon@gmail.com>
2023-05-26 18:03:07 +0200

    Usage of openai to make summaries

--- podcast_downloader/summary.py
+++ podcast_downloader/summary.py
@@ -4,12 +4,18 @@ text from ChatGPT based on the key seconds provided by the user. Here,
 functions could be defined to send the transcription to ChatGPT and
 receive a summary response.
 """
-import requests
+
+import openai
 
 
 # function to request summary from ChatGPT
-def get_summary(text):
-    # send the text to ChatGPT API and receive summary response
-    response = requests.post(url="CHATGPT_API_URL", data={"text": text})
-    summary = response.text
-    return summary
+def get_summary(prompt: str, token: str, engine: str = "text-davinci-003"):
+    # Configure the request to OpenAI
+    openai.api_key = token
+
+    response = openai.Completion.create(
+        engine=engine, prompt=prompt, max_tokens=50, n=1,
+        stop=None, temperature=0.7
+    )
+
+    return response.choices[0].text.strip()
