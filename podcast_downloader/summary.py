f1bc91bca188661583b3117daa89918879b48805
Victor Mas Montserrat <victormasmon@gmail.com>
2023-05-27 00:46:54 +0200

    Using transformers from huggingface

--- podcast_downloader/summary.py
+++ podcast_downloader/summary.py
@@ -1,21 +1,32 @@
 """
 This file would be responsible for requesting a summary of the transcribed
-text from ChatGPT based on the key seconds provided by the user. Here,
-functions could be defined to send the transcription to ChatGPT and
+text from HuggingFace based on the key seconds provided by the user. Here,
+functions could be defined to send the transcription to HuggingFace and
 receive a summary response.
 """
 
-import openai
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 
-# function to request summary from ChatGPT
-def get_summary(prompt: str, token: str, engine: str = "text-davinci-003"):
-    # Configure the request to OpenAI
-    openai.api_key = token
+# function to request summary from HuggingFace
+def get_summary(prompt: str):
 
-    response = openai.Completion.create(
-        engine=engine, prompt=prompt, max_tokens=50, n=1,
-        stop=None, temperature=0.7
-    )
+    # Load the model
+    model_name = 'gpt2'
+    model = GPT2LMHeadModel.from_pretrained(model_name)
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-    return response.choices[0].text.strip()
+    # Sets the mode of how the text will be generated
+    model.eval()
+
+    # Codificar el prompt
+    input_ids = tokenizer.encode(prompt, return_tensors='pt')
+
+    # Generate the text based on the prompt
+    output = model.generate(
+        input_ids, max_length=50,
+        num_return_sequences=1,
+        no_repeat_ngram_size=2)
+
+    # Return the answer
+    print(tokenizer.decode(output[0], skip_special_tokens=True))
